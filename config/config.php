d899b6c91dc29a56109f78896a695c51884358d0
Julio Escamilla <esjuliorafael@gmail.com>
2026-01-09 23:31:53 -0600

    9 de enero 11:31

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 // Define la URL base de tu subdominio
-define('URL_BASE', 'https://rifas.rancholastrojes.com');
-define('URL_PRINCIPAL', 'https://rancholastrojes.com');
+define('URL_BASE', 'https://rifas.rancholastrojes.com.mx');
+define('URL_PRINCIPAL', 'https://rancholastrojes.com.mx');
 
 // Rutas de archivos
 define('PATH_UPLOADS', __DIR__ . '/../assets/uploads/');
